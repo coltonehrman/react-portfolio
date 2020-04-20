5fe3834434d2b16d5284d5ce9ffe7b0007e9f1e2
Colton Ehrman <cehrman@Coltons-MacBook-Pro.local>
2020-04-19 17:55:28 -0700

    Updates

---
+++ precache-manifest.16e8744a7a3d4b4b7ddbb5cc98769765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9ea102260fee8938828ce89a8e16ad",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e8321b988ded1d48953a",
+    "url": "/react-portfolio/static/css/main.d1856583.chunk.css"
+  },
+  {
+    "revision": "4e3eca1f15fc5477ada1",
+    "url": "/react-portfolio/static/js/2.18ce0b32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.18ce0b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8321b988ded1d48953a",
+    "url": "/react-portfolio/static/js/main.faa36639.chunk.js"
+  },
+  {
+    "revision": "b8295b4a7ca704aaf440",
+    "url": "/react-portfolio/static/js/runtime-main.e9b31376.js"
+  }
+]);
